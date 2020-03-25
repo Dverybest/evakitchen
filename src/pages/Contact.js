51d1d1a4da8311db66b721324f0f953952c44eaa
Charles Best <charlesbest8@gmail.com>
2020-03-25 23:44:53 +0100

    updated views

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -41,18 +41,18 @@ const Contact = () => {
                             </div>
                             <div className='col-md-5 px-5 my-5'>
                                  <div >
-                                    <h4 className='d-flex align-content-center justify-content-around ' style={{fontSize:25,color:'#fff'}}>STAY CONNECTED</h4>
-                                    <div className="line row mx-3"></div>
+                                    <h4 className='d-flex align-content-center justify-content-around text-center' style={{fontSize:25,color:'#fff'}}>STAY CONNECTED</h4>
+                                    <div className="line row mx-1"></div>
                                  <div className="d-flex align-content-center justify-content-around my-3">
                                      <span ><a href="https://www.facebook.com/bestinol"><i className="fa fa-facebook-square"></i></a></span>
                                     <span > <a href="https://www.twitter.com/"><i className="fa fa-twitter-square" style={{color:'white'}}></i></a></span>
                                     <span> <a href="https://www.twitter.com/"><i className="fa fa-youtube-play" style={{color:'red'}}></i></a></span>
                                  </div>
                                     <div className="d-flex align-content-center justify-content-center" style={{fontSize:20,color:'#fff'}}>
-                                        <p><i className="fa fa-phone" ></i>  +2347061011343</p>
+                                        <i className="fa fa-phone" ></i>  +2347061011343
                                     </div>
                                     <div className="d-flex align-content-center justify-content-center" style={{fontSize:20,color:'#fff',textAlign:'center'}}>
-                                        <p><i className="fa fa-envelope-o" ></i>  evakitchen@gmail.com</p>
+                                        <i className="fa fa-envelope-o mr-2" ></i>   evakitchen@gmail.com
                                     </div>
                                  </div>
                                     
